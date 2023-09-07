334faf73a0a7525eef60c7f248b6c727e97cac6e
yashinshekh <developeryasin@gmail.com>
2023-09-07 10:01:30 +0545

    upload files

--- handyankauf.py
+++ handyankauf.py
@@ -66,7 +66,7 @@ def uploadtospreadsheet():
 
     spreadsheet_id = spreadsheet_url.split('/')[-2]
 
-    with open(sheet_name+".csv") as csv_file:
+    with open(os.getcwd()+"/"+sheet_name+".csv") as csv_file:
         csv_reader = csv.reader(csv_file)
         rows = list(csv_reader)
 
@@ -97,103 +97,104 @@ if __name__ == '__main__':
     spreadsheet_url = "https://docs.google.com/spreadsheets/d/1KaBHOgUEwVfrUmoMvzsIoH6aIhZdNhlDXquF3lq7NGs/edit#gid=0"
     sheet_name = "handyankauf"
 
-    alreadyscrapped = []
-    firefox_options = Options()
-    firefox_options.headless = True
-    driver = webdriver.Firefox(options=firefox_options)
-
-    with open(sheet_name+".csv","a",newline="",encoding="utf-8") as f:
-        writer = csv.writer(f)
-        writer.writerow(['link','timestamp','brand','title','phone_name','storage','Like new','Very good','Good','In order','Inadequate','EUR -> AUD','EUR -> HKD'])
-
-    scrapped_time = datetime.datetime.now().strftime("%Y-%m-%d (%H:%M)")
-
-    driver.get("https://wise.com/")
-    driver.find_element(By.ID,'tw-calculator-source-select').click()
-    driver.find_element(By.ID,'tw-calculator-source-select-searchbox').send_keys('EUR')
-    ActionChains(driver).send_keys(Keys.ENTER).perform()
-    time.sleep(1)
-    driver.get("https://wise.com/")
-    driver.find_element(By.ID,'tw-calculator-target-select').click()
-    driver.find_element(By.ID,'tw-calculator-target-select-searchbox').send_keys('HKD')
-    ActionChains(driver).send_keys(Keys.ENTER).perform()
-    time.sleep(1)
-    eur_hkd_rate = Selector(text=driver.page_source).xpath('.//*[@class="tw-calculator-breakdown-rate__value"]/text()').extract_first()
-
-    print(eur_hkd_rate)
-
-    driver.find_element(By.ID,'tw-calculator-source-select').click()
-    driver.find_element(By.ID,'tw-calculator-source-select-searchbox').send_keys('EUR')
-    ActionChains(driver).send_keys(Keys.ENTER).perform()
-    time.sleep(1)
-    driver.get("https://wise.com/")
-    driver.find_element(By.ID,'tw-calculator-target-select').click()
-    driver.find_element(By.ID,'tw-calculator-target-select-searchbox').send_keys('AUD')
-    ActionChains(driver).send_keys(Keys.ENTER).perform()
-    time.sleep(1)
-    eur_aud_rate = Selector(text=driver.page_source).xpath('.//*[@class="tw-calculator-breakdown-rate__value"]/text()').extract_first()
-
-    print(eur_aud_rate)
-
-
-    driver.get("https://handyankauf-online.at/")
-
-    ds = Selector(text=a).xpath('.//a').extract()
-    for d in ds:
-        sel1 = Selector(text=d)
-        brand = sel1.xpath('.//a/text()').extract_first()
-        link = sel1.xpath('.//a/@href').extract_first()
-
-        print("Link: "+str(link))
-        driver.get("https://handyankauf-online.at/"+link)
-
-        datas1 = Selector(text=driver.page_source).xpath('.//*[@class="fs10lh1-5 cf1"]').extract()
-        for data1 in datas1:
-            sel2 = Selector(text=data1)
-            link_2 = sel2.xpath('.//a/@href').extract_first()
-            title = sel2.xpath('.//a/text()').extract_first().replace('verkaufen','')
-
-            driver.get("https://handyankauf-online.at/"+link_2)
-
-            # phone_links = Selector(text=driver.page_source).xpath('.//button/@onclick[contains(.,"return x5engine.utils.imPopUpWin")]').extract()
-            phone_links = Selector(text=driver.page_source).xpath('.//button/@onclick[contains(.,"return x5engine.utils.imPopUpWin")] | .//*[@class="fs10lh1-5 cf1"]/a/@href').extract()
-
-            print(phone_links)
-
-            for phone_link in phone_links:
-                phone_link = phone_link.replace("return x5engine.utils.imPopUpWin('",'').replace("','imPopUp', 1000, 562);",'')
-                phone_link = '/'+phone_link if '/script' not in phone_link else phone_link
-                if "https://handyankauf-online.at"+phone_link not in alreadyscrapped:
-                    try:
-                        driver.get("https://handyankauf-online.at"+phone_link)
-
-                        response = Selector(text=driver.page_source)
-
-                        storage = response.xpath('.//h1/text()').extract_first().split()[-1]
-                        phone_name = response.xpath('.//h1/text()').extract_first()
-                        try:
-                            phone_name = phone_name.replace(storage,'')
-                        except:
-                            pass
-
-                        cond_prices = {"Wie neu":'',"Sehr Gut":'','Gut':'','In Ordnung':'','Mangelhaft':''}
-
-                        for condition in ['Wie neu','Sehr Gut','Gut','In Ordnung','Mangelhaft']:
-                            try:
-                                Select(driver.find_element(By.XPATH,'.//*[@name="zustand"]')).select_by_visible_text(condition)
-                                time.sleep(3)
-
-                                cond_prices[condition] = ''.join(Selector(text=driver.page_source).xpath('.//*[@id="price"]/..//text()').extract())
-                            except:
-                                cond_prices[condition] = ''
-
-                        with open(sheet_name+".csv","a",newline="",encoding="utf-8") as f:
-                            writer = csv.writer(f)
-                            writer.writerow(["https://handyankauf-online.at"+phone_link,scrapped_time,brand,title,phone_name,storage]+list(cond_prices.values())+[eur_aud_rate,eur_hkd_rate])
-                            print(["https://handyankauf-online.at"+phone_link,scrapped_time,brand,title,phone_name,storage]+list(cond_prices.values())+[eur_aud_rate,eur_hkd_rate])
-
-                    except:
-                        pass
+    # alreadyscrapped = []
+    # firefox_options = Options()
+    # firefox_options.headless = True
+    # driver = webdriver.Firefox(options=firefox_options)
+    #
+    # with open(os.getcwd()+"/"+sheet_name+".csv","a",newline="",encoding="utf-8") as f:
+    #     writer = csv.writer(f)
+    #     writer.writerow(['link','timestamp','brand','title','phone_name','storage','Like new','Very good','Good','In order','Inadequate','EUR -> AUD','EUR -> HKD'])
+    #
+    # scrapped_time = datetime.datetime.now().strftime("%Y-%m-%d (%H:%M)")
+    #
+    # driver.get("https://wise.com/")
+    # driver.find_element(By.ID,'tw-calculator-source-select').click()
+    # driver.find_element(By.ID,'tw-calculator-source-select-searchbox').send_keys('EUR')
+    # ActionChains(driver).send_keys(Keys.ENTER).perform()
+    # time.sleep(1)
+    # driver.get("https://wise.com/")
+    # driver.find_element(By.ID,'tw-calculator-target-select').click()
+    # driver.find_element(By.ID,'tw-calculator-target-select-searchbox').send_keys('HKD')
+    # ActionChains(driver).send_keys(Keys.ENTER).perform()
+    # time.sleep(1)
+    # eur_hkd_rate = Selector(text=driver.page_source).xpath('.//*[@class="tw-calculator-breakdown-rate__value"]/text()').extract_first()
+    #
+    # print(eur_hkd_rate)
+    #
+    # driver.find_element(By.ID,'tw-calculator-source-select').click()
+    # driver.find_element(By.ID,'tw-calculator-source-select-searchbox').send_keys('EUR')
+    # ActionChains(driver).send_keys(Keys.ENTER).perform()
+    # time.sleep(1)
+    # driver.get("https://wise.com/")
+    # driver.find_element(By.ID,'tw-calculator-target-select').click()
+    # driver.find_element(By.ID,'tw-calculator-target-select-searchbox').send_keys('AUD')
+    # ActionChains(driver).send_keys(Keys.ENTER).perform()
+    # time.sleep(1)
+    # eur_aud_rate = Selector(text=driver.page_source).xpath('.//*[@class="tw-calculator-breakdown-rate__value"]/text()').extract_first()
+    #
+    # print(eur_aud_rate)
+    #
+    #
+    # driver.get("https://handyankauf-online.at/")
+    #
+    # ds = Selector(text=a).xpath('.//a').extract()
+    # for d in ds:
+    #     sel1 = Selector(text=d)
+    #     brand = sel1.xpath('.//a/text()').extract_first()
+    #     link = sel1.xpath('.//a/@href').extract_first()
+    #
+    #     print("Link: "+str(link))
+    #     driver.get("https://handyankauf-online.at/"+link)
+    #
+    #     datas1 = Selector(text=driver.page_source).xpath('.//*[@class="fs10lh1-5 cf1"]').extract()
+    #     for data1 in datas1:
+    #         sel2 = Selector(text=data1)
+    #         link_2 = sel2.xpath('.//a/@href').extract_first()
+    #         title = sel2.xpath('.//a/text()').extract_first().replace('verkaufen','')
+    #
+    #         driver.get("https://handyankauf-online.at/"+link_2)
+    #
+    #         # phone_links = Selector(text=driver.page_source).xpath('.//button/@onclick[contains(.,"return x5engine.utils.imPopUpWin")]').extract()
+    #         phone_links = Selector(text=driver.page_source).xpath('.//button/@onclick[contains(.,"return x5engine.utils.imPopUpWin")] | .//*[@class="fs10lh1-5 cf1"]/a/@href').extract()
+    #
+    #         print(phone_links)
+    #
+    #         for phone_link in phone_links:
+    #             phone_link = phone_link.replace("return x5engine.utils.imPopUpWin('",'').replace("','imPopUp', 1000, 562);",'')
+    #             phone_link = '/'+phone_link if '/script' not in phone_link else phone_link
+    #             if "https://handyankauf-online.at"+phone_link not in alreadyscrapped:
+    #                 try:
+    #                     driver.get("https://handyankauf-online.at"+phone_link)
+    #
+    #                     response = Selector(text=driver.page_source)
+    #
+    #                     storage = response.xpath('.//h1/text()').extract_first().split()[-1]
+    #                     phone_name = response.xpath('.//h1/text()').extract_first()
+    #                     try:
+    #                         phone_name = phone_name.replace(storage,'')
+    #                     except:
+    #                         pass
+    #
+    #                     cond_prices = {"Wie neu":'',"Sehr Gut":'','Gut':'','In Ordnung':'','Mangelhaft':''}
+    #
+    #                     for condition in ['Wie neu','Sehr Gut','Gut','In Ordnung','Mangelhaft']:
+    #                         try:
+    #                             Select(driver.find_element(By.XPATH,'.//*[@name="zustand"]')).select_by_visible_text(condition)
+    #                             time.sleep(3)
+    #
+    #                             cond_prices[condition] = ''.join(Selector(text=driver.page_source).xpath('.//*[@id="price"]/..//text()').extract())
+    #                         except:
+    #                             cond_prices[condition] = ''
+    #
+    #                     with open(os.getcwd()+"/"+sheet_name+".csv","a",newline="",encoding="utf-8") as f:
+    #                         writer = csv.writer(f)
+    #                         writer.writerow(["https://handyankauf-online.at"+phone_link,scrapped_time,brand,title,phone_name,storage]+list(cond_prices.values())+[eur_aud_rate,eur_hkd_rate])
+    #                         print(["https://handyankauf-online.at"+phone_link,scrapped_time,brand,title,phone_name,storage]+list(cond_prices.values())+[eur_aud_rate,eur_hkd_rate])
+    #
+    #                 except:
+    #                     pass
+    #
+    # driver.close()
 
     uploadtospreadsheet()
-    driver.close()
